214587a574ef9e931d18d86d09a3ed4ae3625b8e
pierre@pcmolinaro.name <pierre@pcmolinaro.name>
2018-11-24 15:25:05 +0100

    First release

---
+++ src/ACAN2515Tiny.h
@@ -0,0 +1,176 @@
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// A CAN driver for MCP2515
+// by Pierre Molinaro
+// https://github.com/pierremolinaro/acan2515
+//
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+#pragma once
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+#include <ACANBuffer8.h>
+#include <ACAN2515TinySettings.h>
+#include <SPI.h>
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+class ACAN2515Mask {
+
+//--- Default constructor
+  public: ACAN2515Mask (void) :
+  mSIDH (0),
+  mSIDL (0),
+  mEID8 (0),
+  mEID0 (0) {
+  }
+
+//--- Properties
+  public: uint8_t mSIDH ;
+  public: uint8_t mSIDL ;
+  public: uint8_t mEID8 ;
+  public: uint8_t mEID0 ;
+} ;
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+ACAN2515Mask standard2515Mask (const uint16_t inIdentifier,
+                               const uint8_t inByte0,
+                               const uint8_t inByte1) ;
+
+//······················································································································
+
+ACAN2515Mask extended2515Mask (const uint32_t inIdentifier) ;
+
+//······················································································································
+
+ACAN2515Mask standard2515Filter (const uint16_t inIdentifier,
+                                 const uint8_t inByte0,
+                                 const uint8_t inByte1) ;
+
+//······················································································································
+
+ACAN2515Mask extended2515Filter (const uint32_t inIdentifier) ;
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+class ACAN2515AcceptanceFilter {
+  public: typedef void (*tCallBackRoutine) (const CANMessage & inMessage) ;
+  public: const ACAN2515Mask mMask ;
+  public: const tCallBackRoutine mCallBack ;
+} ;
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+class ACAN2515Tiny {
+//--- Constructor
+  public: ACAN2515Tiny (const uint8_t inCS,  // CS input of MCP2515
+                        SPIClass & inSPI, // Hardware SPI object
+                        const uint8_t inINT) ; // INT output of MCP2515
+
+
+//--- Initialisation: returns 0 if ok, otherwise see error codes below
+  public: uint16_t begin (const ACAN2515TinySettings & inSettings,
+                          void (* inInterruptServiceRoutine) (void)) ;
+
+  public: uint16_t begin (const ACAN2515TinySettings & inSettings,
+                          void (* inInterruptServiceRoutine) (void),
+                          const ACAN2515Mask inRXM0,
+                          const ACAN2515AcceptanceFilter inAcceptanceFilters [],
+                          const uint8_t inAcceptanceFilterCount) ;
+
+  public: uint16_t begin (const ACAN2515TinySettings & inSettings,
+                          void (* inInterruptServiceRoutine) (void),
+                          const ACAN2515Mask inRXM0,
+                          const ACAN2515Mask inRXM1,
+                          const ACAN2515AcceptanceFilter inAcceptanceFilters [],
+                          const uint8_t inAcceptanceFilterCount) ;
+
+//--- Error codes returned by begin
+  public: static const uint16_t kNoMCP2515                   = 1 <<  0 ;
+  public: static const uint16_t kTooFarFromDesiredBitRate    = 1 <<  1 ;
+  public: static const uint16_t kInconsistentBitRateSettings = 1 <<  2 ;
+  public: static const uint16_t kINTPinIsNotAnInterrupt      = 1 <<  3 ;
+  public: static const uint16_t kISRIsNull                   = 1 <<  4 ;
+  public: static const uint16_t kRequestedModeTimeOut        = 1 <<  5 ;
+  public: static const uint16_t kAcceptanceFilterArrayIsNULL = 1 << 6 ;
+  public: static const uint16_t kOneFilterMaskRequiresOneOrTwoAcceptanceFilters = 1 << 7 ;
+  public: static const uint16_t kTwoFilterMasksRequireThreeToSixAcceptanceFilters = 1 << 8 ;
+
+//--- Receiving messages
+  public: bool available (void) ;
+  public: bool receive (CANMessage & outFrame) ;
+  public: typedef void (*tFilterMatchCallBack) (const uint8_t inFilterIndex) ;
+  public: bool dispatchReceivedMessage (const tFilterMatchCallBack inFilterMatchCallBack = NULL) ;
+
+//--- Handling messages to send and receiving messages
+  public: void isr (void) ;
+  private: void handleTXBInterrupt (void) ;
+  private: void handleRXBInterrupt (void) ;
+
+ //--- Properties
+  private: SPIClass & mSPI ;
+  private: const SPISettings mSPISettings ;
+  private: const uint8_t mCS ;
+  private: const uint8_t mINT ;
+
+//--- Receive buffer
+  private: ACANBuffer8 mReceiveBuffer ;
+
+//--- Call back function array
+  private: ACANCallBackRoutine mCallBackFunctionArray [6] ;
+
+//--- Transmitting messages
+  public: bool tryToSend (const CANMessage & inMessage) ;
+
+//--- Driver transmit buffer
+  private: ACANBuffer8 mTransmitBuffer ;
+  private: bool mTXBIsFree ;
+
+  public: inline uint8_t transmitBufferSize (void) const {
+    return mTransmitBuffer.size () ;
+  }
+
+  public: inline uint8_t transmitBufferCount (void) const {
+    return mTransmitBuffer.count () ;
+  }
+
+  public: inline uint8_t transmitBufferPeakCount (void) const {
+    return mTransmitBuffer.peakCount () ;
+  }
+  private: void internalSendMessage (const CANMessage & inFrame) ;
+
+//--- Private methods
+  private: inline void select (void) { digitalWrite (mCS, LOW) ; }
+  private: inline void unselect (void) { digitalWrite (mCS, HIGH) ; }
+
+  private: uint16_t beginWithoutFilterCheck (const ACAN2515TinySettings & inSettings,
+                                             void (* inInterruptServiceRoutine) (void),
+                                             const ACAN2515Mask inRXM0,
+                                             const ACAN2515Mask inRXM1,
+                                             const ACAN2515AcceptanceFilter inAcceptanceFilters [],
+                                             const uint8_t inAcceptanceFilterCount) ;
+
+  private: uint16_t internalBeginOperation (const ACAN2515TinySettings & inSettings,
+                                            const ACAN2515Mask inRXM0,
+                                            const ACAN2515Mask inRXM1,
+                                            const ACAN2515AcceptanceFilter inAcceptanceFilters [],
+                                            const uint8_t inAcceptanceFilterCount) ;
+
+  private: void write2515Register (const uint8_t inRegister, const uint8_t inValue) ;
+  private: uint8_t read2515Register (const uint8_t inRegister) ;
+  private: uint8_t read2515Status (void) ;
+  private: uint8_t read2515RxStatus (void) ;
+  private: void bitModify2515Register (const uint8_t inRegister, const uint8_t inMask, const uint8_t inData) ;
+  private: void setupMaskRegister (const ACAN2515Mask inMask, const uint8_t inRegister) ;
+
+//--- MCP2515 controller state
+  public: uint8_t receiveErrorCounter (void) ;
+  public: uint8_t transmitErrorCounter (void) ;
+
+//--- No Copy
+  private: ACAN2515Tiny (const ACAN2515Tiny &) ;
+  private: ACAN2515Tiny & operator = (const ACAN2515Tiny &) ;
+} ;
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
---
+++ src/ACAN2515TinySettings.h
@@ -0,0 +1,100 @@
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+// A CAN driver for MCP2515
+// by Pierre Molinaro
+// https://github.com/pierremolinaro/acan2515Tiny
+//
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+#pragma once
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+#include <stdint.h>
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
+class ACAN2515TinySettings {
+//--- Enumerations
+  public: typedef enum {NormalMode, ListenOnlyMode, LoopBackMode} ACAN2515RequestedMode ;
+
+//--- Constructor for a given baud rate
+  public: explicit ACAN2515TinySettings (const uint32_t inQuartzFrequency, // In Hertz
+                                         const uint32_t inDesiredBitRate,
+                                         const uint32_t inTolerancePPM = 1000) ;
+//--- Constructor with explicit bit settings
+  public: explicit ACAN2515TinySettings (const uint32_t inQuartzFrequency, // In Hertz
+                                         const uint8_t inBitRatePrescaler,  // 1...64
+                                         const uint8_t inPropagationSegment, // 1...8
+                                         const uint8_t inPhaseSegment1, // 1...8
+                                         const uint8_t inPhaseSegment2, // 2...8
+                                         const uint8_t inSJW) ; // 1...4
+
+//--- CAN bit timing
+  public: const uint32_t mQuartzFrequency ;
+  public: uint32_t mDesiredBitRate = mQuartzFrequency / 64 ; // In kb/s
+  public: uint8_t mPropagationSegment = 5 ; // 1...8
+  public: uint8_t mPhaseSegment1 = 5 ; // 1...8
+  public: uint8_t mPhaseSegment2 = 5 ; // 2...8
+  public: uint8_t mSJW = 4 ; // 1...4
+  public: uint8_t mBitRatePrescaler = 32 / (1 + mPropagationSegment + mPhaseSegment1 + mPhaseSegment2) ; // 1...64
+  public: bool mTripleSampling = false ; // true --> triple sampling, false --> single sampling
+  public: bool mBitRateClosedToDesiredRate = true ; // The above configuration is correct
+
+
+//--- One shot mode
+//      true --> Enabled; messages will only attempt to transmit one time
+//      false --> Disabled; messages will reattempt transmission if required
+  public: bool mOneShotModeEnabled = false ;
+
+
+//--- Rollover Enable Bit (is set to the BUKT bit of the RXB0CTRL register)
+//       true  --> RXB0 message will roll over and be written to RXB1 if RXB0 is full
+//       false --> Rollover is disabled
+  public : bool mRolloverEnable = true ;
+
+
+//--- Requested mode
+  public: ACAN2515RequestedMode mRequestedMode = NormalMode ;
+
+
+//--- Receive buffer size
+  public: uint8_t mReceiveBufferSize = 32 ; // 0 ... 128
+
+
+//--- Transmit buffer size
+  public: uint8_t mTransmitBufferSize = 16 ; // 0 ... 128
+
+
+//--- Compute actual bit rate
+  public: uint32_t actualBitRate (void) const ;
+
+//--- Exact bit rate ?
+  public: bool exactBitRate (void) const ;
+
+//--- Distance between actual bit rate and requested bit rate (in ppm, part-per-million)
+  public: uint32_t ppmFromDesiredBitRate (void) const ;
+
+//--- Distance of sample point from bit start (in ppc, part-per-cent, denoted by %)
+  public: uint32_t samplePointFromBitStart (void) const ;
+
+//--- Bit settings are consistent ? (returns 0 if ok)
+  public: uint16_t CANBitSettingConsistency (void) const ;
+
+//--- Constants returned by CANBitSettingConsistency
+  public: static const uint16_t kBitRatePrescalerIsZero                 = 1 <<  0 ;
+  public: static const uint16_t kBitRatePrescalerIsGreaterThan64        = 1 <<  1 ;
+  public: static const uint16_t kPropagationSegmentIsZero               = 1 <<  2 ;
+  public: static const uint16_t kPropagationSegmentIsGreaterThan8       = 1 <<  3 ;
+  public: static const uint16_t kPhaseSegment1IsZero                    = 1 <<  4 ;
+  public: static const uint16_t kPhaseSegment1IsGreaterThan8            = 1 <<  5 ;
+  public: static const uint16_t kPhaseSegment2IsLowerThan2              = 1 <<  6 ;
+  public: static const uint16_t kPhaseSegment2IsGreaterThan8            = 1 <<  7 ;
+  public: static const uint16_t kPhaseSegment1Is1AndTripleSampling      = 1 <<  8 ;
+  public: static const uint16_t kSJWIsZero                              = 1 <<  9 ;
+  public: static const uint16_t kSJWIsGreaterThan4                      = 1 << 10 ;
+  public: static const uint16_t kSJWIsGreaterThanOrEqualToPhaseSegment2 = 1 << 11 ;
+  public: static const uint16_t kPhaseSegment2IsGreaterThanPSPlusPS1    = 1 << 12 ;
+} ;
+
+//——————————————————————————————————————————————————————————————————————————————————————————————————————————————————————
+
